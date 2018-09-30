f0990a52dc6061e616c54cb4154b24c33ffaba23
daniel <dcavalcanticosta@gmail.com>
2018-09-30 19:06:50 -0300

    Código do primeiro módulo

---
+++ angular-web-site-institucional/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { RouterModule } from '@angular/router';
+import { ContatoModule } from './contato/contato.module';
+import { AppRoutingModule } from './app-routing.module';
+import { NossaVisaoModule } from './nossa-visao/nossa-visao.module';
+import { QuemSomosModule } from './quem-somos/quem-somos.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    ContatoModule,
+    NossaVisaoModule,
+    QuemSomosModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
