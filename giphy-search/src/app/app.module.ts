6349bd97d676dbf088e95eb7b15295f06eb02a8d
daniel <dcavalcanticosta@gmail.com>
2018-09-30 20:47:13 -0300

    Criação do componente que irá fazer as requisições para a API do giphy.

--- giphy-search/src/app/app.module.ts
+++ giphy-search/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GiphySearchManualComponent } from './giphy/giphy-search-manual/giphy-search-manual.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GiphySearchManualComponent
   ],
   imports: [
     BrowserModule
---
+++ giphy-search/src/app/giphy/giphy-search-manual/giphy-search-manual.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'giphy-search-manual',
+    templateUrl: 'giphy-search-manual.component.html'
+})
+
+export class GiphySearchManualComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
