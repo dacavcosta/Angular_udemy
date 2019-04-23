d71ba8905d465ad29e987d2eabda3a3b614579ca
daCav <dcavalcanticosta@gmail.com>
2019-04-22 22:06:58 -0300

    Adicionando o arquivo raiz.modulo com o comentário sobre o exemplo

--- angular-avancado/src/app/compartilhando-estados-com-services/raiz.module.ts
+++ angular-avancado/src/app/compartilhando-estados-com-services/raiz.module.ts
@@ -16,3 +16,10 @@ import { Nvl1Module } from './nvl1/nvl1.module';
   providers: [RaizService]
 })
 export class RaizModule { }
+
+/*
+    Este exemplo foi feito apenas para observar o comportamento do valor do conteúdo do service com
+  relação aos componentes e com a detecção de mudanças destes componentes. Não é recomendado o uso
+  de serviços para compartilhar informações com componentes desta forma. Existem outra forma de
+  utilizar services com componentes, mas utilizando observables, que é o mais recomendado.
+*/
